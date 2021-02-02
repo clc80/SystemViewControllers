ebd96e8432261c6c89e680179dd46360d2c3f2aa
Claudia Maciel <clc_80@yahoo.com>
2021-02-01 18:38:40 -0800

    Share with the Activity Controller

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shareButtonPressed(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    @IBAction func safariButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func emailButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
